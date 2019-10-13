67d15d970ec92816cf1bf2eed52586dcc284b134
Ryan Harper <ryan@192.168.178.1>
2019-10-13 16:03:33 +0100

    Database file Added

--- config/dbdump.sql
+++ config/dbdump.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.26, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.27, for Linux (x86_64)
 --
 -- Host: localhost    Database: wjgarments
 -- ------------------------------------------------------
--- Server version	5.7.26-0ubuntu0.18.04.1
+-- Server version	5.7.27-0ubuntu0.18.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,7 +26,7 @@ CREATE TABLE `newsletter` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `email` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,6 +35,7 @@ CREATE TABLE `newsletter` (
 
 LOCK TABLES `newsletter` WRITE;
 /*!40000 ALTER TABLE `newsletter` DISABLE KEYS */;
+INSERT INTO `newsletter` VALUES (1,'ryan1230tenerife@gmail.com'),(2,''),(3,'ryan1230tenerife@gmail.com');
 /*!40000 ALTER TABLE `newsletter` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -50,7 +51,7 @@ CREATE TABLE `product_color` (
   `product_id` varchar(200) DEFAULT NULL,
   `product_color` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -59,6 +60,7 @@ CREATE TABLE `product_color` (
 
 LOCK TABLES `product_color` WRITE;
 /*!40000 ALTER TABLE `product_color` DISABLE KEYS */;
+INSERT INTO `product_color` VALUES (1,'Jacket','Blue'),(2,'Demin Jacket','Blue'),(3,'Demin Jacket','Blue'),(4,'Jacket','Blue'),(5,'Shoes','Blue'),(6,'Shoes','Blue'),(7,'sdfds','Green'),(8,'sdfdsa','Blue'),(9,'Jacket','Blue'),(10,'Jacket','Blue'),(11,'Jacket','Blue');
 /*!40000 ALTER TABLE `product_color` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -98,7 +100,7 @@ CREATE TABLE `product_size` (
   `product_id` varchar(200) DEFAULT NULL,
   `product_size` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -107,6 +109,7 @@ CREATE TABLE `product_size` (
 
 LOCK TABLES `product_size` WRITE;
 /*!40000 ALTER TABLE `product_size` DISABLE KEYS */;
+INSERT INTO `product_size` VALUES (1,'Jacket','M'),(2,'Demin Jacket','M'),(3,'Demin Jacket','M'),(4,'Jacket','M'),(5,'Shoes','M'),(6,'Shoes','M'),(7,'sdfds','M'),(8,'sdfdsa','S'),(9,'Jacket','S'),(10,'Jacket','S'),(11,'Jacket','S');
 /*!40000 ALTER TABLE `product_size` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -186,7 +189,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Ryan','ryan1230tenerife@gmail.com','$2y$10$KQYVFSgQmZ84Gg6pg/rCwOt7g24LPBsKcXBhB/ReeTAHVdgaDTcCG');
+INSERT INTO `users` VALUES (1,'Ryan','ryan@mail.com','$2y$10$/1wb66KdBIXruOF90RXbwe5C11PFeKxJmkCVdfTiCHbyC1hxvg7/m');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -223,4 +226,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-06-26 20:07:28
+-- Dump completed on 2019-10-13 16:02:53
