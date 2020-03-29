a3fcbe126fcb5f5a18d4f43ed8baf77e70d870af
Ryan <34920194+ryan1230x@users.noreply.github.com>
2020-03-29 23:01:45 +0100

    Update dbh.php

--- PDOconfig/dbh.php
+++ PDOconfig/dbh.php
@@ -1,8 +1,8 @@
 <?php
-$username = "u122331496_ryan_shop";
-$db_name = "u122331496_online_shop";
+$username = "";
+$db_name = "";
 $host = "localhost";
-$db_password = "r.harp3r";
+$db_password = "";
 $dsn = "mysql:host=$host;dbname=$db_name;charset=utf8mb4";
 
 // Connection to database
