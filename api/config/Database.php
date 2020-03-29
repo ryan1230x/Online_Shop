68baed6f4a4c78e7c0734ed9db58723d3516339b
Ryan <34920194+ryan1230x@users.noreply.github.com>
2020-03-29 23:02:39 +0100

    Update Database.php

--- api/config/Database.php
+++ api/config/Database.php
@@ -6,9 +6,9 @@ class Database {
 # Create function to connect to database	
 public function connect() {
 	$this->conn = null;
-	$dsn = "mysql:host=localhost;dbname=u122331496_online_shop;charset=utf8mb4";
-	$user = "u122331496_ryan_shop";
-	$pass = "r.harp3r";
+	$dsn = "mysql:host=localhost;dbname=...;charset=utf8mb4";
+	$user = "";
+	$pass = "";
 	try {
 			$this->conn = new PDO($dsn, $user, $pass);			
 
